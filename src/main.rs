9f164d382de50109794f22263e6721be280fbe01
nicolasgarza <nicolas.garza@gmail.com>
2023-12-24 01:25:28 -0600

    more stuff added

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use clap::Parser;
 use std::fs;
 
-/// My Simple Program
+
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
-    /// List of input values
     #[clap(value_parser)]
     path: String,
 }
@@ -15,11 +14,20 @@ fn main() {
     
     match fs::read_dir(cli.path) {
         Ok(entries) => {
-            entries
-                .filter_map(Result::ok) 
-                .for_each(|entry| {
-                    println!("{:?}", entry.path());
-                });
+            for entry in entries.filter_map(Result::ok) {
+                let path = entry.path().to_string_lossy().to_string();
+                println!("Path: {:?}", path);
+
+                if let Ok(metadata) = entry.metadata() {
+                    if metadata.is_file() {
+                        println!("File extension: {:?}", path.rsplit_once('.').map_or("", |(_, right)| right));
+                        println!("{} bytes", metadata.len());
+                    } else if metadata.is_dir() {
+                        println!("Folder");
+                        println!("Permissions: {:?}", metadata.permissions());
+                    }
+                }
+            }
         },
         Err(e) => println!("Error: {}", e),
     };
